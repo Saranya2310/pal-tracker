7a64bef52a8726ac050ffcf58f0bb3a71734ff93
Saranya2310 <saranya-19.r-19@cognizant.com>
2020-04-14 17:42:51 +0530

    PAL-Spring MVC

--- src/test/java/test/pivotal/pal/tracker/TimeEntryControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/TimeEntryControllerTest.java
@@ -1,12 +1,12 @@
 package test.pivotal.pal.tracker;
 
-import io.pivotal.pal.tracker.TimeEntry;
 import io.pivotal.pal.tracker.TimeEntryController;
-import io.pivotal.pal.tracker.TimeEntryRepository;
+import io.pivotal.pal.tracker.bean.TimeEntry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import io.pivotal.pal.tracker.dao.TimeEntryRepository;
 
 import java.time.LocalDate;
 import java.util.List;
